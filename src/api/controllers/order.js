5d21a26a8b7377d21ba01ef6b13742074e3ccb4b
andrew work <andrew-work@Mac-mini-andrew.local>
2024-05-31 21:08:30 +0300

    orders services

--- src/api/controllers/order.js
+++ src/api/controllers/order.js
@@ -1,15 +1,44 @@
+import mongoose from 'mongoose'
 import Order from '../models/order.js'
 
-const getAll = async (req, res) => {
+const getAllActive = async (req, res) => {
   try {
-    const orders = await Order.find().exec()
+    const orders = await Order.find({
+      userId: req.params.userId,
+      delivered: false,
+    }).exec()
 
     return res.json({ success: true, orders })
   } catch (error) {
     console.log(error)
 
     return res.status(400).json({
-      message: 'Помилка при полученні замовлень',
+      message: 'Помилка при полученні активних замовлень',
+      success: false,
+    })
+  }
+}
+
+const getAllNotActive = async (req, res) => {
+  try {
+    const orders = await Order.aggregate([
+      {
+        $match: {
+          userId: new mongoose.Types.ObjectId(req.params.userId),
+          $or: [
+            { status: { $regex: 'Доставлено', $options: 'i' } },
+            { status: { $regex: 'Скасовано', $options: 'i' } },
+          ],
+        },
+      },
+    ])
+
+    return res.json({ success: true, orders })
+  } catch (error) {
+    console.log(error)
+
+    return res.status(400).json({
+      message: 'Помилка при полученні не активних замовлень',
       success: false,
     })
   }
@@ -36,4 +65,4 @@ const create = async (req, res) => {
   }
 }
 
-export const OrderController = { getAll, create }
+export const OrderController = { getAllActive, getAllNotActive, create }
--- src/api/routes/order.js
+++ src/api/routes/order.js
@@ -6,7 +6,18 @@ import { OrderController } from '../controllers/order.js'
 
 const router = Router()
 
-router.get('/orders', [authMiddleware], OrderController.getAll)
+router.get(
+  '/orders/active/:userId',
+  [authMiddleware],
+  OrderController.getAllActive
+)
+
+router.get(
+  '/orders/history/:userId',
+  [authMiddleware],
+  OrderController.getAllNotActive
+)
+
 router.post('/orders', [authMiddleware], OrderController.create)
 
 export default router
