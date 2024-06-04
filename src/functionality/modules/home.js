abc3c6c41d6193106aaa6d537070a9a267379ad3
andrew work <andrew-work@Mac-mini-andrew.local>
2024-06-04 19:45:15 +0300

    v

--- src/functionality/modules/home.js
+++ src/functionality/modules/home.js
@@ -23,7 +23,18 @@ const actualDOM = () => {
     if (responseProducts.products.length) {
       DOM.discountedSection.classList.add('active')
 
-      const productListHtml = responseProducts.products.map(product => {
+      const productListHtml = [
+        ...responseProducts.products,
+        ...responseProducts.products,
+        ...responseProducts.products,
+        ...responseProducts.products,
+        ...responseProducts.products,
+        ...responseProducts.products,
+        ...responseProducts.products,
+        ...responseProducts.products,
+        ...responseProducts.products,
+        ...responseProducts.products,
+      ].map(product => {
         return `
           <div class="swiper-slide">
             ${productCard({ product })}
@@ -56,4 +67,27 @@ new Swiper('#home-swiper', {
 new Swiper('#discounted-swiper', {
   slidesPerView: 4,
   spaceBetween: 20,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.2,
+      spaceBetween: 10,
+    },
+    350: {
+      slidesPerView: 1.3,
+      spaceBetween: 10,
+    },
+    600: {
+      slidesPerView: 1.7,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    930: {
+      slidesPerView: 3,
+    },
+    1240: {
+      slidesPerView: 4,
+    },
+  },
 })
